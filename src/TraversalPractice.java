181ce6a3e625777069572dae2b3e93ac05ded115
Auberon Lopez <auberon.lopez@greenriver.edu>
2026-06-03 09:40:36 -0700

    Merge in non-return traversal practice

--- src/TraversalPractice.java
+++ src/TraversalPractice.java
@@ -1,5 +1,47 @@
 public class TraversalPractice {
     /**
+     * Prints only the values of nodes of a tree that are strictly larger than
+     * 1000, traversing post-order.
+     * 
+     * Each value is printed on a separate line. Values less than or equal to
+     * 1000 are not printed.
+     * 
+     * If the node is null, nothing is printed.
+     * 
+     * @param node the root of the tree
+     */
+    public static void printLargeNodesPostOrder(TreeNode node) {
+
+    }
+
+    /**
+     * Prints only the negative values of nodes of a tree, traversing in-order.
+     * 
+     * Each value is printed on a separate line. Positive/zero values are not
+     * printed.
+     * 
+     * If the node is null, nothing is printed.
+     * 
+     * @param node the root of the tree
+     */
+    public static void printNegativeNodesInOrder(TreeNode node) {
+
+    }
+
+    /**
+     * Prints only the odd values of nodes of a tree, traversing in pre-order.
+     * 
+     * Each value is printed on a separate line. Even values are not printed.
+     * 
+     * If the node is null, nothing is printed.
+     * 
+     * @param node the root of the tree
+     */
+    public static void printOddNodesPreOrder(TreeNode node) {
+
+    }
+    
+    /**
      * Returns the sum of the leaf nodes in a tree.
      * 
      * If the input node is null, returns 0
